abc126d8648502b7d875a854a8dab30de769a88d
prabalab <40490211+prabalab@users.noreply.github.com>
2025-02-07 09:26:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.post("/send-mail", async (req, res) => {
   try {
     let transporter = nodemailer.createTransport({
       service: "gmail",
+      secure : true,
+      port : 465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
